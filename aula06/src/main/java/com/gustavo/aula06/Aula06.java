61bd04d45ca116c68875ac327d159a64e2419036
Gustavo Camilotti <05166@duque.br.local>
2025-08-25 19:54:04 -0300

    Primeiro commit da aplicação

---
+++ aula06/src/main/java/com/gustavo/aula06/Aula06.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gustavo.aula06;
+
+/**
+ *
+ * @author 05166
+ */
+public class Aula06 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
